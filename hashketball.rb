95d4a4e653215343b8eb56110163793330f9ec4a
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-15 01:30:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,9 @@ end
 def num_points_scored(player)
   game_hash.each { |key, value|
     value.each { |k, v|
-      
+      if v[:player] == player
+        return v[:points]
+      end
     }
   }
 end
